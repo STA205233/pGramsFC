bcce49152267cf4232644dbf909b6a525feb7c34
ShotaArai <shota.arai@phys.s.u-tokyo.ac.jp>
2025-02-03 23:09:05 +0900

    add Socket test code in Python

--- tools/socket_test/socket_test.py
+++ tools/socket_test/socket_test.py
@@ -1,19 +1,17 @@
+#! /usr/bin/env python3
 import socket
+import sys
 
-def main():
+
+def main(host, port):
     # Create a socket object
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # Get local machine name
-    host = ""
-
-    port = 0
 
     # Connection to hostname on the port.
     s.connect((host, port))
     try:
         while (True):
-        # Receive no more than 1024 bytes
+            # Receive no more than 1024 bytes
             msg = s.recv(1024)
             print(f"Received: {len(msg)} bytes")
             for byte in msg:
@@ -21,3 +19,10 @@ def main():
     except KeyboardInterrupt:
         pass
     s.close()
+
+
+if __name__ == '__main__':
+    if (len(sys.argv) != 3):
+        print(f"Usage: {sys.argv[0]} <host> <port>")
+        sys.exit(1)
+    main(sys.argv[1], int(sys.argv[2]))
