16e2d38b24f83d359c347052ec1500e376af69dc
ShotaArai <shota.arai@phys.s.u-tokyo.ac.jp>
2025-12-18 02:49:49 +0900

    added mysql port parameter into interpret_telemetry.rb

--- examples/interpret_telemetry/interpret_telemetry.rb
+++ examples/interpret_telemetry/interpret_telemetry.rb
@@ -32,7 +32,9 @@ class MyApp < ANL::ANLApp
     chain GRAMSBalloon::InterpretTelemetry, "InterpretHKTelemetry"
     with_parameters(receiver_module_name: "ReceiveTelemetry_HK", run_ID_filename: ENV["HOME"] + "/settings/run_id/run_id_ground.txt", save_telemetry: false, num_telem_per_file: 1000, chatter: 2, telemetry_type: "HK")
     chain GRAMSBalloon::PushToMySQL
-    with_parameters(HubHKInterpreter_name: "InterpretHKTelemetry", host: ENV["PGRAMS_MYSQL_HOST"], user: ENV["PGRAMS_MYSQL_USER"], password: ENV["PGRAMS_MYSQL_PASSWD"], database: "pgrams", check_exist: true, chatter: 4)
+    with_parameters(HubHKInterpreter_name: "InterpretHKTelemetry", host: ENV["PGRAMS_MYSQL_HOST"], user: ENV["PGRAMS_MYSQL_USER"], password: ENV["PGRAMS_MYSQL_PASSWD"], port: ENV["PGRAMS_MYSQL_PORT"].to_i, database: "pgrams", check_exist: true, chatter: 4)
+    chain GRAMSBalloon::Sleep
+    with_parameters(sleep_sec: 1)
   end
 end
 
