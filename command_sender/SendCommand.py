c88107eab8844f70e4f2f1fdc649e1c338ae7847
ShotaArai <shota.arai@phys.s.u-tokyo.ac.jp>
2025-12-18 07:44:31 -0500

    change path of send_command in SendCommand.py

--- command_sender/SendCommand.py
+++ command_sender/SendCommand.py
@@ -5,9 +5,9 @@ import argparse
 import tkinter as tk
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-
+import os
 
 if __name__ == "__main__":
-    executable_prefix = "/Users/shotaarai/software/pGramsFC/command_sender/build/send_command"
+    executable_prefix = f"{os.getenv('HOME')}/software/pGramsFC/command_sender/build/send_command"
     gui_manager = pGramsComSender.GUIManager.GUIRunner(executable_prefix=executable_prefix)
     gui_manager.run({"Hub": ["Dummy1", "Dummy2", "Prepare Shutdown", "Exec Shutdown", "Prepare Shutdown", "Exec Restart", "Reset Error", "Set Link"], "Orc": ["Exec CPU Restart", "Exec CPU Shutdown", "Boot All DAQ", "Shutdown All DAQ", "CPU Status"]})
