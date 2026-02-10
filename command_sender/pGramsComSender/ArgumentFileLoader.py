99e07a3bf504626960eb930ffab89dbcd75e7289
ShotaArai <shota.arai@phys.s.u-tokyo.ac.jp>
2026-02-10 15:51:35 +0900

    Copy argument file if it is loaded.

--- command_sender/pGramsComSender/ArgumentFileLoader.py
+++ command_sender/pGramsComSender/ArgumentFileLoader.py
@@ -1,9 +1,18 @@
 from os import PathLike
+from shutil import copy2
+from datetime import datetime
+from pathlib import Path
 
 class ArgumentFileLoader:
-    def __init__(self) -> None:
+    def __init__(self, logging) -> None:
         self.args = []
-    def load(self, filepath: int | str | bytes | PathLike[str] | PathLike[bytes]):
+        self.logging = logging
+    def load(self, filepath:str | PathLike[str] , copy=True):
+        filepath = Path(filepath)
+        if copy:
+            copy_filename = datetime.now().strftime(f"{filepath.parent}/{filepath.stem}_%Y%m%d_%H%M%S.txt")
+            copy2(filepath, copy_filename)
+            self.logging.info(f"Copied argument file {filepath} to {copy_filename}")
         with open(filepath, "r") as f:
             lines = f.readlines()
         self.args = [int(line.strip()) for line in lines if line.strip() != ""]
