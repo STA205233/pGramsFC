1cb83091e753605c6b295c87aacb02991b314c05
ShotaArai <shota.arai@phys.s.u-tokyo.ac.jp>
2026-07-09 21:52:39 +0900

    Introduce VDBDataStore class for DataBase treatment

--- onboard/source/modules/include/ConvertHubHKTelemetry.hh
+++ onboard/source/modules/include/ConvertHubHKTelemetry.hh
@@ -1,21 +1,23 @@
 #ifndef GB_ConvertHubHKTelemetry_hh
 #define GB_ConvertHubHKTelemetry_hh 1
+#include "ConvertedHubHKTelemetry.hh"
 #include "InterpretTelemetry.hh"
+#include "VDBDataStore.hh"
 #include "anlnext/BasicModule.hh"
-#include <string>
 #include <memory>
+#include <string>
 namespace gramsballoon::pgrams {
 class ConvertedHubHKTelemetry;
 class HubHKTelemetry;
-
 
 /**
  * @brief Convert received HubHKTelemetry
  * @author Shota Arai
  * @date 2026-06-16 | Shota Arai | First version (v1.0)
+ * @date 2026-07-09 | Shota Arai | Inherited from VDBDataStore
  */
-class ConvertHubHKTelemetry: public anlnext::BasicModule {
-  DEFINE_ANL_MODULE(ConvertHubHKTelemetry, 1.0);
+class ConvertHubHKTelemetry: public anlnext::BasicModule, VDBDataStore {
+  DEFINE_ANL_MODULE(ConvertHubHKTelemetry, 1.1);
   ENABLE_PARALLEL_RUN();
 
 public:
@@ -29,6 +31,19 @@ public:
   anlnext::ANLStatus mod_define() override;
   anlnext::ANLStatus mod_initialize() override;
   anlnext::ANLStatus mod_analyze() override;
+
+  void pushToDBSink(DBFieldSink *sink) const override {
+    if (singleton_self()->convertedTelemetry_) {
+      singleton_self()->convertedTelemetry_->serialize(sink);
+    }
+  }
+
+  void initializeDBTableInSink(DBFieldSink *sink, const std::string &table_name) const override {
+    if (singleton_self()->convertedTelemetry_) {
+      singleton_self()->convertedTelemetry_->initializeDBTable(sink, table_name);
+    }
+  }
+  bool hasData() const override { return interpretTelemetry_->hasData(); }
 
 private:
   const InterpretTelemetry *interpretTelemetry_ = nullptr;
