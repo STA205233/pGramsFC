e4216dbdc7b1118c59e94d3be27c1139db036ebd
ShotaArai <shota.arai@phys.s.u-tokyo.ac.jp>
2026-07-26 21:03:25 +0900

    Delete unnecessary modules

--- examples/get_all_sensors_NEU/get_all_sensors_NEU.rb
+++ examples/get_all_sensors_NEU/get_all_sensors_NEU.rb
@@ -21,10 +21,6 @@ class MyApp < ANL::ANLApp
     with_parameters(channel: 4)
     chain GRAMSBalloon::MeasureTemperatureWithRTDSensorByMHADC, "MeasureTemperatureWithRTDSensorByMHADC_6"
     with_parameters(channel: 5)
-    chain GRAMSBalloon::EncodedSerialCommunicator, "CompresorManager"
-    with_parameters(filename: "/dev/ttyUSB0", baudrate:9600)
-    chain GRAMSBalloon::GetCompressorData
-    with_parameters(EncodedSerialCommunicator_name: "CompresorManager")
     chain GRAMSBalloon::PressureGaugeManager, "PressureCommunicator_1"
     with_parameters(filename: "/dev/ttyUSB1", baudrate:115200)
     chain GRAMSBalloon::GetPressure, "GetPressure_1"
--- onboard/rubyext/GRAMSBalloon.i
+++ onboard/rubyext/GRAMSBalloon.i
@@ -208,11 +208,6 @@ class SendPacketByMQTT : public anlnext::BasicModule
 public:
   SendPacketByMQTT();
 };
-class MeasureOrientationByMHADC: public anlnext::BasicModule {
-public:
-  MeasureOrientationByMHADC();
-};
-
 class DetectErrorCallbackFromDAQ: public anlnext::BasicModule {
 public:
   DetectErrorCallbackFromDAQ();
