c3441df4162421e2235a1d3375d17276bef64f56
ShotaArai <shota.arai@phys.s.u-tokyo.ac.jp>
2026-08-18 15:43:04 +0900

    delete MT feature

--- onboard/source/core/include/ToFBiasController.hh
+++ onboard/source/core/include/ToFBiasController.hh
@@ -3,13 +3,9 @@
 #include "EncodedSerialCommunication.hh"
 #include "HKDataSaver.hh"
 #include "ToFBiasChannel.hh"
-#include <atomic>
-#include <condition_variable>
 #include <cstdint>
 #include <memory>
-#include <mutex>
 #include <ostream>
-#include <thread>
 namespace gramsballoon::pgrams {
 
 /**
@@ -19,41 +15,21 @@ namespace gramsballoon::pgrams {
  */
 class ToFBiasController: public EncodedSerialCommunication, public HKDataSaver<uint8_t> {
   // for telemetry
-  static constexpr int NUM_ADC = 128;
-  static constexpr int NUM_TEMP = 32;
-  static constexpr int NUM_DAC = 128;
-  static constexpr int NUM_DATA = NUM_ADC * 4 + NUM_TEMP * 4 + NUM_DAC * 3;
+  static constexpr int NUM_DATA = 536;
   // for command
   static constexpr int NUM_DCDC_SETTING = 8;
   static constexpr int NUM_TEMP_SETTING = 16;
-  static constexpr int NUM_TRIM_SETTING = 128;
 
 public:
   ToFBiasController();
-  ToFBiasController(const std::string &serial_path);
+  ToFBiasController(const std::string& serial_path);
   virtual ~ToFBiasController();
   int enableDataStream();
   int disableDataStream();
-  int setTrimVoltage(uint32_t channel0, uint32_t channel1, uint32_t channel2, uint32_t channel3, int voltage);
-  int setDefaultTemperature(uint16_t channel, int temperature);
-  int enableTemperatureControl();
-  int disableTemperatureControl();
+  int queryFullOutput();
   int enableDCDC(uint8_t channel);
   int disableDCDC(uint8_t channel);
-  std::ostream &printData(std::ostream &os);
-
-  std::array<uint16_t, NUM_ADC> AdcData() {
-    std::unique_lock<std::mutex> lock(mutexForData_);
-    return adcData_;
-  }
-  std::array<uint16_t, NUM_TEMP> TempData() {
-    std::unique_lock<std::mutex> lock(mutexForData_);
-    return tempData_;
-  }
-  std::array<uint16_t, NUM_DAC> DacData() {
-    std::unique_lock<std::mutex> lock(mutexForData_);
-    return dacData_;
-  }
+  std::ostream& printData(std::ostream& os);
 
   void setTimeOut(int sec, int usec) {
     timeOut_.tv_sec = sec;
@@ -61,22 +37,16 @@ public:
   }
 
   bool HasError() {
-    return hasError_.load();
+    return hasError_;
   }
 
   int initialize() override {
-    if (isRunning_.load() == 1) {
-      isRunning_.store(2);
-      cond_.notify_all();
-      thread_->join();
-      thread_.reset();
-    }
-    const int ret = SerialCommunication::initialize();
+    const int ret = EncodedSerialCommunication::initialize();
     if (ret < 0) {
-      hasError_.store(true);
+      hasError_ = true;
     }
     else {
-      hasError_.store(false);
+      hasError_ = false;
     }
     return ret;
   }
@@ -95,26 +65,16 @@ protected:
   using EncodedSerialCommunication::waitForReceivable;
   using EncodedSerialCommunication::WriteDataWithTimeout;
 
-  int WriteDataWithTimeoutForToF(const std::string &data, timeval &timeout);
-  int readDataWithInterpret();
+  int sendCommand(const std::string& data, timeval& timeout);
+  int readData();
   void interpret(const char *dataStr);
 
 private:
-  std::array<uint16_t, NUM_ADC> adcData_;
   char dataStr_[NUM_DATA];
-  std::array<uint16_t, NUM_TEMP> tempData_;
-  std::array<uint16_t, NUM_DAC> dacData_;
-  std::condition_variable cond_;
-  std::atomic_int isRunning_; // 0: off, 1: running, 2: no thread
-  std::atomic_bool hasError_;
-  std::unique_ptr<std::thread> thread_;
-  std::mutex mutexForData_;
-  std::mutex mutexForCond_;
-  std::mutex mutexForComm_;
+  bool hasError_;
   size_t index_;
   std::unique_ptr<ToFBiasChannel<NUM_DCDC_SETTING>> channelForDCDC_ = nullptr;
   std::unique_ptr<ToFBiasChannel<NUM_TEMP_SETTING>> channelForTemp_ = nullptr;
-  std::unique_ptr<ToFBiasChannel<NUM_TRIM_SETTING>> channelForTrim_ = nullptr;
   timeval timeOut_ = {0, 100000};
 };
 
--- onboard/source/core/src/ToFBiasController.cc
+++ onboard/source/core/src/ToFBiasController.cc
@@ -1,46 +1,49 @@
 #include "ToFBiasController.hh"
 #include <charconv>
-#include <thread>
 #define TOF_BIAS_DEBUG 0
 
 namespace gramsballoon::pgrams {
-ToFBiasController::ToFBiasController(const std::string &serial_path) : EncodedSerialCommunication(serial_path, B115200, O_RDWR), HKDataSaver<uint8_t>(100000, "tof_bias_data", NUM_DATA * sizeof(uint16_t)), isRunning_(2), hasError_(false), index_(0) {
-  channelForDCDC_ = std::make_unique<ToFBiasChannel<8>>();
-  channelForTemp_ = std::make_unique<ToFBiasChannel<16>>();
-  channelForTrim_ = std::make_unique<ToFBiasChannel<128>>();
+ToFBiasController::ToFBiasController(const std::string& serial_path) : EncodedSerialCommunication(serial_path, B115200, O_RDWR), HKDataSaver<uint8_t>(100000, "tof_bias_data", NUM_DATA * sizeof(uint16_t)), hasError_(false), index_(0) {
 }
-ToFBiasController::ToFBiasController() : ToFBiasController("/dev/ttyACM0") {}
+ToFBiasController::ToFBiasController() : ToFBiasController("/dev/ttyUSB0") {
+  disableDataStream();
+}
 ToFBiasController::~ToFBiasController() {
   disableDataStream();
-  if (thread_) {
-    isRunning_.store(2);
-    cond_.notify_all();
-    thread_->join();
-  }
 }
 
-int ToFBiasController::WriteDataWithTimeoutForToF(const std::string &data, timeval &timeout) {
-  const int ret = WriteDataWithTimeout(data, timeout);
-  if (ret < 0) {
-    hasError_.store(true);
+int ToFBiasController::sendCommand(const std::string& data, timeval& timeout) {
+  {
+    const int ret = WriteDataWithTimeout(data, timeout);
+    if (ret < 0) {
+      hasError_ = true;
+      std::cerr << "TOFBiasController::sendCommand: failed to write command code" << ret << std::endl;
+      return ret;
+    }
+    hasError_ = false;
   }
-  else {
-    hasError_.store(false);
+  {
+    const int ret = ReadDataWithTimeout(dataStr_, sizeof(dataStr_), timeout);
+    if (ret < 0) {
+      hasError_ = true;
+      std::cerr << "TOFBiasController::sendCommand: failed to receive command code" << ret << std::endl;
+      return ret;
+    }
   }
-  return ret;
+  std::cerr << "TOFBiasController::sendCommand: some error happens" << std::endl;
+  return -1;
 }
 
-int ToFBiasController::readDataWithInterpret() {
+int ToFBiasController::readData() {
   int index = 0;
   {
-    std::unique_lock<std::mutex> commLock(mutexForComm_);
     while (index < NUM_DATA - 1) {
       const int ret = ReadDataWithTimeout(&dataStr_[index], NUM_DATA - index, timeOut_);
       if (ret < 0) {
 #if TOF_BIAS_DEBUG > 0
         std::cerr << "error in communication with ToF Bias" << std::endl;
 #endif
-        hasError_.store(true);
+        hasError_ = true;
         return ret;
       }
       if (ret == 0) {
@@ -49,7 +52,7 @@ int ToFBiasController::readDataWithInterpret() {
 #endif
         return 0;
       }
-      hasError_.store(false);
+      hasError_ = false;
       index += ret;
     }
     if (static_cast<size_t>(index) != NUM_DATA) {
@@ -57,88 +60,29 @@ int ToFBiasController::readDataWithInterpret() {
       return -1;
     }
   }
-  interpret(dataStr_);
   return index;
 }
 
+int ToFBiasController::queryFullOutput() {
+  const int ret = sendCommand("p", timeOut_);
+  if (ret < 0) {
+    std::cerr << "Failed to write data" << std::endl;
+    return ret;
+  }
+  return ret;
+}
+
 int ToFBiasController::enableDataStream() {
-  int ret;
-  {
-    std::unique_lock<std::mutex> commLock(mutexForComm_);
-    ret = WriteDataWithTimeoutForToF("data on", timeOut_);
-    if (ret < 0) {
-      std::cerr << "Failed to write data" << std::endl;
-      return ret;
-    }
-  }
-  if (isRunning_.load() == 2) {
-    isRunning_.store(1);
-    thread_ = std::make_unique<std::thread>([&]() {
-      while (true) {
-        const int is_running = isRunning_.load();
-        if (is_running == 0) {
-#if TOF_BIAS_DEBUG >= 0
-          std::cout << "Stopping thread..." << std::endl;
-#endif
-          std::unique_lock<std::mutex> lock(mutexForCond_);
-          cond_.wait(lock, [&] { return isRunning_.load() != 0; });
-        }
-        else if (is_running == 2) {
-#if TOF_BIAS_DEBUG >= 0
-          std::cout << "killing thread..." << std::endl;
-#endif
-          break;
-        }
-#if TOF_BIAS_DEBUG > 0
-        std::cout << "Running thread..." << std::endl;
-#endif
-        readDataWithInterpret();
-      }
-    });
-  }
-  else {
-    isRunning_.store(1);
-    cond_.notify_one();
+  const int ret = sendCommand("data on", timeOut_);
+  if (ret < 0) {
+    std::cerr << "Failed to write data" << std::endl;
+    return ret;
   }
   return ret;
 }
 int ToFBiasController::disableDataStream() {
-  std::unique_lock<std::mutex> commLock(mutexForComm_);
-  const int ret = WriteDataWithTimeoutForToF("data off ", timeOut_);
-  if (ret > 0) {
-    isRunning_.store(0);
-  }
+  const int ret = sendCommand("data off ", timeOut_);
   return ret;
-}
-
-int ToFBiasController::setTrimVoltage(uint32_t channel0, uint32_t channel1, uint32_t channel2, uint32_t channel3, int voltage) {
-  channelForTrim_->setValue(channel0, channel1, channel2, channel3);
-  std::string ch = channelForTrim_->serialize();
-  if (ch.empty()) {
-    return -1;
-  }
-  std::unique_lock<std::mutex> commLock(mutexForComm_);
-  return WriteDataWithTimeoutForToF("trim " + ch + " " + std::to_string(voltage) + " ", timeOut_);
-}
-
-int ToFBiasController::setDefaultTemperature(uint16_t channel, int temperature) {
-  channelForTemp_->setValue(channel);
-  std::string ch = channelForTemp_->serialize();
-  if (ch.empty()) {
-    return -1;
-  }
-  std::unique_lock<std::mutex> commLock(mutexForComm_);
-  return WriteDataWithTimeoutForToF("temp " + ch + " " + std::to_string(temperature) + " ", timeOut_);
-}
-
-int ToFBiasController::enableTemperatureControl() {
-  std::unique_lock<std::mutex> commLock(mutexForComm_);
-  return WriteDataWithTimeoutForToF("tenable on ", timeOut_);
-}
-
-int ToFBiasController::disableTemperatureControl() {
-  std::unique_lock<std::mutex> commLock(mutexForComm_);
-  return WriteDataWithTimeoutForToF("tenable off ", timeOut_);
 }
 
 int ToFBiasController::enableDCDC(uint8_t channel) {
@@ -147,8 +91,7 @@ int ToFBiasController::enableDCDC(uint8_t channel) {
   if (ch.empty()) {
     return -1;
   }
-  std::unique_lock<std::mutex> commLock(mutexForComm_);
-  return WriteDataWithTimeoutForToF("denable " + ch + " on ", timeOut_);
+  return sendCommand("denable " + ch + " on ", timeOut_);
 }
 
 int ToFBiasController::disableDCDC(uint8_t channel) {
@@ -157,13 +100,11 @@ int ToFBiasController::disableDCDC(uint8_t channel) {
   if (ch.empty()) {
     return -1;
   }
-  std::unique_lock<std::mutex> commLock(mutexForComm_);
-  return WriteDataWithTimeoutForToF("denable " + channelForDCDC_->serialize() + " off ", timeOut_);
+  return sendCommand("denable " + channelForDCDC_->serialize() + " off ", timeOut_);
 }
 
-std::ostream &ToFBiasController::printData(std::ostream &os) {
-  std::unique_lock<std::mutex> commLock(mutexForComm_);
-  const int ret = WriteDataWithTimeoutForToF("p ", timeOut_);
+std::ostream& ToFBiasController::printData(std::ostream& os) {
+  const int ret = sendCommand("p ", timeOut_);
   if (ret < 0) {
     os << "Failed to send print command: " << ret;
     return os;
@@ -178,21 +119,4 @@ std::ostream &ToFBiasController::printData(std::ostream &os) {
   return os;
 }
 
-void ToFBiasController::interpret(const char *dataStr) {
-  const char *p = dataStr;
-  std::unique_lock<std::mutex> lock(mutexForData_);
-  for (auto &v: adcData_) {
-    std::from_chars(p, p + 4, v, 16);
-    p += 4;
-  }
-  for (auto &v: tempData_) {
-    std::from_chars(p, p + 4, v, 16);
-    p += 4;
-  }
-  for (auto &v: dacData_) {
-    std::from_chars(p, p + 3, v, 16);
-    p += 3;
-  }
-  ++index_;
-}
 } // namespace gramsballoon::pgrams
